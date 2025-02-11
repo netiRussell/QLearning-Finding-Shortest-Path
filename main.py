053447a02b1661951592bb03e96c74d760309eea
Russell / Ruslan <forworkneti@gmail.com>
2025-02-11 15:09:01 -0800

    Dataset generator implemented

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 # TODO: compare the models using fixed destination node and the same environment. Train both with the same time and compare the accuracy.
 
 import numpy as np
+import math
+from supplemental import is_terminal_state
+from supplemental import get_starting_location 
+from supplemental import get_next_action 
+from supplemental import get_next_location
+from supplemental import get_shortest_path 
 
-# Settings
-environment_rows = 11
-environment_columns = 11
+"""
+  Configurations
+"""
+environment_rows = 50
+environment_columns = 50
 
 # Defining q values holder
 # a "Q-value" represents the expected future reward an agent can receive by taking a specific action ("a") in a particular state ("s") within an environment
@@ -16,82 +24,27 @@ q_values = np.zeros((environment_rows, environment_columns, 4))
 #numeric action codes: 0 = up, 1 = right, 2 = down, 3 = left
 actions = ['up', 'right', 'down', 'left']
 
-
 # Rewards
 rewards = np.full((environment_rows, environment_columns), -1)
-rewards[0, 5] = 100 # Destination
+destination = 60
 
-# Defining a function that determines if the specified location is a terminal state
-def is_terminal_state(current_row_index, current_column_index):
-  #if the reward for this location is -1, then it is not a terminal state (i.e., it is a 'white square')
-  if rewards[current_row_index, current_column_index] == -1.:
-    return False
-  else:
-    return True
-
-# Defining a function that will choose a random, non-terminal starting location
-def get_starting_location():
-  #get a random row and column index
-  current_row_index = np.random.randint(environment_rows)
-  current_column_index = np.random.randint(environment_columns)
-  #continue choosing random row and column indexes until a non-terminal state is identified
-  #(i.e., until the chosen state is a 'white square').
-  while is_terminal_state(current_row_index, current_column_index):
-    current_row_index = np.random.randint(environment_rows)
-    current_column_index = np.random.randint(environment_columns)
-  return current_row_index, current_column_index
-
-# Defining an epsilon greedy algorithm that will choose which action to take next (i.e., where to move next)
-def get_next_action(current_row_index, current_column_index, epsilon):
-  #if a randomly chosen value between 0 and 1 is less than epsilon, 
-  #then choose the most promising value from the Q-table for this state.
-  if np.random.random() < epsilon:
-    # Choose the action with the highest q value for this state
-    return np.argmax(q_values[current_row_index, current_column_index])
-  else: #choose a random action
-    return np.random.randint(4)
-
-# Defining a function that will get the next location based on the chosen action
-def get_next_location(current_row_index, current_column_index, action_index):
-  new_row_index = current_row_index
-  new_column_index = current_column_index
-  if actions[action_index] == 'up' and current_row_index > 0:
-    new_row_index -= 1
-  elif actions[action_index] == 'right' and current_column_index < environment_columns - 1:
-    new_column_index += 1
-  elif actions[action_index] == 'down' and current_row_index < environment_rows - 1:
-    new_row_index += 1
-  elif actions[action_index] == 'left' and current_column_index > 0:
-    new_column_index -= 1
-  return new_row_index, new_column_index
-
-# Defining a function that will get the shortest path between any location within the warehouse that 
-# Agent is allowed to travel and the item packaging location.
-def get_shortest_path(start_row_index, start_column_index):
-  #return immediately if this is an invalid starting location
-  if is_terminal_state(start_row_index, start_column_index):
-    return []
-  else: #if this is a 'legal' starting location
-    current_row_index, current_column_index = start_row_index, start_column_index
-    shortest_path = []
-    shortest_path.append([current_row_index, current_column_index])
-    #continue moving along the path until we reach the goal (i.e., the item packaging location)
-    while not is_terminal_state(current_row_index, current_column_index):
-      #get the best action to take
-      action_index = get_next_action(current_row_index, current_column_index, 1.)
-      #move to the next location on the path, and add the new location to the list
-      current_row_index, current_column_index = get_next_location(current_row_index, current_column_index, action_index)
-      shortest_path.append([current_row_index, current_column_index])
-    return shortest_path
-
+# Destination
+rewards[math.floor(destination / environment_rows), destination % environment_rows] = 100
 
 # Defining training parameters
 epsilon = 0.9 #the percentage of time when we should take the best action (instead of a random action)
 discount_factor = 0.9 #discount factor for future rewards
 learning_rate = 0.9 #the rate at which the AI agent should learn
 
-#run through 1000 training episodes
-for episode in range(1000):
+
+
+
+"""
+  Training
+"""
+
+# Run through N training episodes
+for episode in range(10):
   #get the starting location for this episode
   row_index, column_index = get_starting_location()
 
@@ -118,6 +71,6 @@ print('Training complete!')
 
 
 # Test
-print(get_shortest_path(3, 9)) #starting at row 3, column 9
-print(get_shortest_path(5, 0)) #starting at row 5, column 0
-print(get_shortest_path(9, 5)) #starting at row 9, column 5
+# print(get_shortest_path(3, 9)) #starting at row 3, column 9
+# print(get_shortest_path(5, 0)) #starting at row 5, column 0
+# print(get_shortest_path(9, 5)) #starting at row 9, column 5
---
+++ supplemental.py
@@ -0,0 +1,66 @@
+"""
+  Here are the supplemental functions needed for the main program
+"""
+
+# Defining a function that determines if the specified location is a terminal state
+def is_terminal_state(current_row_index, current_column_index):
+  #if the reward for this location is -1, then it is not a terminal state (i.e., it is a 'white square')
+  if rewards[current_row_index, current_column_index] == -1.:
+    return False
+  else:
+    return True
+
+# Defining a function that will choose a random, non-terminal starting location
+def get_starting_location():
+  #get a random row and column index
+  current_row_index = np.random.randint(environment_rows)
+  current_column_index = np.random.randint(environment_columns)
+  #continue choosing random row and column indexes until a non-terminal state is identified
+  #(i.e., until the chosen state is a 'white square').
+  while is_terminal_state(current_row_index, current_column_index):
+    current_row_index = np.random.randint(environment_rows)
+    current_column_index = np.random.randint(environment_columns)
+  return current_row_index, current_column_index
+
+# Defining an epsilon greedy algorithm that will choose which action to take next (i.e., where to move next)
+def get_next_action(current_row_index, current_column_index, epsilon):
+  #if a randomly chosen value between 0 and 1 is less than epsilon, 
+  #then choose the most promising value from the Q-table for this state.
+  if np.random.random() < epsilon:
+    # Choose the action with the highest q value for this state
+    return np.argmax(q_values[current_row_index, current_column_index])
+  else: #choose a random action
+    return np.random.randint(4)
+
+# Defining a function that will get the next location based on the chosen action
+def get_next_location(current_row_index, current_column_index, action_index):
+  new_row_index = current_row_index
+  new_column_index = current_column_index
+  if actions[action_index] == 'up' and current_row_index > 0:
+    new_row_index -= 1
+  elif actions[action_index] == 'right' and current_column_index < environment_columns - 1:
+    new_column_index += 1
+  elif actions[action_index] == 'down' and current_row_index < environment_rows - 1:
+    new_row_index += 1
+  elif actions[action_index] == 'left' and current_column_index > 0:
+    new_column_index -= 1
+  return new_row_index, new_column_index
+
+# Defining a function that will get the shortest path between any location within the warehouse that 
+# Agent is allowed to travel and the item packaging location.
+def get_shortest_path(start_row_index, start_column_index):
+  #return immediately if this is an invalid starting location
+  if is_terminal_state(start_row_index, start_column_index):
+    return []
+  else: #if this is a 'legal' starting location
+    current_row_index, current_column_index = start_row_index, start_column_index
+    shortest_path = []
+    shortest_path.append([current_row_index, current_column_index])
+    #continue moving along the path until we reach the goal (i.e., the item packaging location)
+    while not is_terminal_state(current_row_index, current_column_index):
+      #get the best action to take
+      action_index = get_next_action(current_row_index, current_column_index, 1.)
+      #move to the next location on the path, and add the new location to the list
+      current_row_index, current_column_index = get_next_location(current_row_index, current_column_index, action_index)
+      shortest_path.append([current_row_index, current_column_index])
+    return shortest_path
